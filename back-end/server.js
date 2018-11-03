021b61b2e174e28ada47e4e471dd378d7924becc
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-11-03 12:58:38 -0400

    play keno 20 balls implemented

--- back-end/server.js
+++ back-end/server.js
@@ -23,10 +23,68 @@ server.get('/htmls', (req, res) => {
 server.get('/play', (req, res) => {
   const game = {
     total: 1000,
-    balls: [67,76,12,1,10,43,24,65,64,73,45,38,19,93]
+    balls: getNumbers(20,80)
   };
 
   res.status(200).json(game);
+});
+
+function isEqual(a1, a2) {
+  let i = a1.length;
+  if (i != a2.length) return false;
+  while (i--) {
+      if (a1[i] !== a2[i]) return false;
+  }
+  return true;
+}
+
+function getNumbers(set, maxNumber){
+  let allNumbers = [];
+  let ballsNumber = [];
+
+  //Create the grid numbers 1-70
+  for(let i = 1; i <= maxNumber ; i++)
+  {
+    allNumbers.push(false);
+  }
+  
+	for(let i = 1; i <= set ; i++){
+		let index = Math.floor(Math.random() * 70) + 1;
+		if(allNumbers[index-1] === false ){
+			ballsNumber.push(index);
+			allNumbers[index-1] = true;
+		} else{
+			i--;
+		}
+  }
+
+  return ballsNumber.sort((a,b) => a-b)
+  // return ballsNumber
+}
+
+
+server.get('/mega', (req, res) => {
+  const mega = [5,28,62,65,70]
+  let ballsNumber = [];
+  let megaNumber = [];
+  let counter = 0;
+  let match = false;
+  let tickets = [];
+
+  do {
+    ballsNumber = getNumbers(5,70);
+    megaNumber = getNumbers(1,25);
+    counter++;
+    match = isEqual(mega,ballsNumber)
+    if(match){
+      console.log(ballsNumber + " " + counter);
+      match = isEqual([5],megaNumber);
+    }
+    // tickets.push({ballsNumber, megaNumber, match, counter});
+  }while( match === false && counter < 1000000)
+  // console.log(match, counter)
+  tickets.push({ballsNumber, megaNumber, match, counter});
+  res.status(200).json(tickets);
 });
 
 const tasks = [];
